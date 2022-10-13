b344678266ce036ab3ca2e760c196ae0ba4fa6b9
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2022-10-14 00:24:08 +0800

    移动端适配

--- src/components/Herder/index.jsx
+++ src/components/Herder/index.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     return(
         <div className='header-container'>
             <Row type="flex" justify='center' align='middle' style={{height:"100%"}}>
-                <Col xs={16} sm={18} md={12} lg={12} xl={12} >
+                <Col xs={14} sm={18} md={12} lg={12} xl={12} >
                     <Link to='/home'><span className='header-logo'>YuZzzze</span></Link>
                     <span className='header-text'>这是React练习的个人博客</span>
                 </Col>
@@ -29,13 +29,20 @@ const Header = () => {
                     </Menu>    
                 </Col>
                 <Col xs={6} sm={4} md={3} lg={3} xl={2} className="login_container">
-                    {userData? <Button type='primary' className='login_botton login_item' onClick={()=>{navigate('/articleEdit')}}>
+                    {userData? <Button type='primary' className='btn login_item' onClick={()=>{navigate('/articleEdit')}}>
                         开始创作
-                    </Button> : ''}     
+                    </Button> : ''}
                 </Col>
-                <Col xs={2} sm={2} md={1} lg={1} xl={1} className="login_container">
-                    {userData? <AvatarInfo/> : <Button type='primary' className='login_botton' onClick={()=>{navigate('/login')}}><span>登录</span></Button>}
-                </Col>
+                {userData ?
+                    <Col xs={2} sm={2} md={1} lg={1} xl={1} className="login_container">
+                        <AvatarInfo/>
+                    </Col> :
+                    <Col xs={4} sm={2} md={1} lg={1} xl={1} className="login_container">
+                        <Button type='primary' className='btn login_botton' onClick={()=>{navigate('/login')}}><span>登录</span></Button>
+                    </Col>
+                }
+
+
             </Row>
         </div>
     )
